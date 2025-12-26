c58b2e8a48cd74efc45fe0709824dbb124751a52
강경래 <kyungrae2002@gmail.com>
2025-12-26 13:51:39 +0900

    수정 4
    모듈 오류

---
+++ src/app/my-page/(about-post)/scraps/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function ScrapsPage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <p className="text-gray-500">스크랩 페이지</p>
+    </div>
+  );
+}
